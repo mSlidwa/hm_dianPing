9ca4239964958b9dfa291461bed83be9e2ca2571
lijiayuan6 <lijiayuan6@yonyou.com>
2024-09-11 15:19:55 +0800

    使用乐观锁避免超卖现象

--- src/main/java/com/hmdp/entity/RedisIdWorker.java
+++ src/main/java/com/hmdp/entity/RedisIdWorker.java
@@ -1,18 +1,21 @@
 package com.hmdp.entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class RedisIdWorker {
-
     private static final long BEGIN_TIMETIMESTAMP = 1640995200L;
+   @Autowired
+    private static StringRedisTemplate redisTemplate;
 
-    private StringRedisTemplate redisTemplate;
-
-    public long nextId(String keyPrefix){
+    public static long nextId(String keyPrefix){
         //1.生成时间戳31魏
         LocalDateTime now = LocalDateTime.now();
         long epochSecond = now.toEpochSecond(ZoneOffset.UTC);
