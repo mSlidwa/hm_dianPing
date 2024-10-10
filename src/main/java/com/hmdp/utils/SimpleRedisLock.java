91dede462c7f44a08c8f24c34c66f682d31f2dfb
lijiayuan6 <lijiayuan6@yonyou.com>
2024-10-10 16:53:15 +0800

    在释放redis锁中，为防止判断与删除出现原子性问题，利用lua脚本将两个操作合并为一个操作

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -2,10 +2,15 @@ package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
 public class SimpleRedisLock {
@@ -13,6 +18,16 @@ public class SimpleRedisLock {
     private static String LOCK="lock:";
 
     private static String uuid= UUID.randomUUID().toString(true)+"-";
+
+    //提前读取lua文件，以便每次直接使用
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        //静态资源在静态代码块中初始化
+        UNLOCK_SCRIPT=new DefaultRedisScript<>();
+        //ClassPathResource就是默认的resource文件，这里指定寻找路径
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+    }
+
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate){
         this.stringRedisTemplate=stringRedisTemplate;
@@ -31,15 +46,19 @@ public class SimpleRedisLock {
         return Boolean.TRUE.equals(result);
     }
 
+//    public void unLock(){
+//        long id = Thread.currentThread().getId();
+//        Long userId = UserHolder.getUser().getId();
+//        String key=LOCK+userId;
+//        String value = stringRedisTemplate.opsForValue().get(key);
+//        if(value.equals(uuid+id)){
+//            stringRedisTemplate.delete(key);
+//        }
+//    }
     public void unLock(){
-        long id = Thread.currentThread().getId();
-        Long userId = UserHolder.getUser().getId();
-        String key=LOCK+userId;
-        String value = stringRedisTemplate.opsForValue().get(key);
-        if(value.equals(uuid+id)){
-            stringRedisTemplate.delete(key);
+       stringRedisTemplate.execute(UNLOCK_SCRIPT,
+               Collections.singletonList(LOCK+UserHolder.getUser().getId()),
+               uuid+Thread.currentThread().getId());
         }
-    }
-
 
 }
